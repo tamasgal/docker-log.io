a09ccebac4aee3d9b879c1adc9ec4e5c8b49a255
Tamas Gal <himself@tamasgal.com>
2017-09-09 16:02:48 +0200

    Add build step

--- run.sh
+++ run.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# build the image
+docker build -t log.io .
+
 # log.io server
-docker run -d -p 28777:28777 -p 8082:28778 -v $(pwd)/config:/home/logio/.log.io --name logio-server tamasgal/log.io-server:0.1
+docker run -d \
+    -p 28777:28777 \
+    -p 8082:28778 \
+    -v $(pwd)/config:/home/logio/.log.io \
+    --name logio-server \
+    log.io
 
 # log.io harvester
-docker run -d -v $(pwd)/config:/home/logio/.log.io -v $(pwd)/logs:/logs --link logio-server:logio-server tamasgal/log.io-server:0.1 log.io-harvester
+docker run -d \
+    -v $(pwd)/config:/home/logio/.log.io \
+    -v $(pwd)/logs:/logs \
+    --link logio-server:logio-server \
+    --name logio-harvester \
+    log.io \
+    log.io-harvester
