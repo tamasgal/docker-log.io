5424ed478d2e445578a2242a5a506fee0ddd8f0b
Tamas Gal <himself@tamasgal.com>
2017-09-09 19:04:51 +0200

    Cosmetics

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # build the image
+echo "Building the Docker image"
 docker build -t log.io .
 
 # log.io server
+echo "Starting log.io server"
 docker run -d \
     -p 28777:28777 \
     -p 8082:28778 \
@@ -12,6 +14,7 @@ docker run -d \
     log.io
 
 # log.io harvester
+echo "Starting log.io harvester"
 docker run -d \
     -v $(pwd)/config:/home/logio/.log.io \
     -v $(pwd)/logs:/logs \
@@ -19,3 +22,5 @@ docker run -d \
     --name logio-harvester \
     log.io \
     log.io-harvester
+
+echo "The log.io service is now up and running."
